ca568e97b1c7b91b5e854d9377659431e034fac6
morgan <mc@m-labs.hk>
2024-09-09 16:14:45 +0800

    libsigrok4dsl: add lib for scopehal sigrok bridge

--- flake.nix
+++ flake.nix
@@ -197,6 +197,39 @@
         installFlags = [ "DESTDIR=$(out)/bin" ];
       };
 
+      # from https://github.com/NixOS/nixpkgs/blob/ab70b01c83dd5ba876d8d79ef5cba24ef185c8c9/pkgs/applications/science/electronics/dsview/libsigrok4dsl.nix
+      libsigrok4dsl = pkgs.stdenv.mkDerivation {
+        pname = "libsigrok4dsl";
+        version = "1.12";
+
+        src = pkgs.fetchFromGitHub {
+          owner = "DreamSourceLab";
+          repo = "DSView";
+          rev = "5a9481fd0697d66ce5ce0e46a8d233125e6cb5ac";
+          hash = "sha256-4sEseH5OmWsesNj+c+RuAu6Oj4yn8TibaA8MnKLo7h4=";
+        };
+
+        postUnpack = ''
+          export sourceRoot=$sourceRoot/libsigrok4DSL
+        '';
+
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          autoreconfHook
+        ];
+
+        buildInputs = with pkgs; [
+          glib
+          libzip
+          libserialport
+          libusb1
+          libftdi
+          systemd
+          check
+          alsa-lib
+        ];
+      };
+
     in
     {
       packages.x86_64-linux = {
