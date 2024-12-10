f09d4c5af4c953c1f5a0290595d982917ad46176
morgan <chengtz414@gmail.com>
2024-12-10 20:33:48 +0800

    tracker: add tui real-time satellite tracking
    rust: bump to 1.81 for fs:exist

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
         system = "x86_64-linux";
         overlays = [ (import rust-overlay) ];
       };
-      rust = pkgs.rust-bin.stable."1.80.0".default.override {
+      rust = pkgs.rust-bin.stable."1.81.0".default.override {
         extensions = [ "rust-src" ];
         targets = [ ];
       };
@@ -65,6 +65,20 @@
         };
 
         cargoHash = "sha256-zd9iJxYtBUo10S3yej4Xm8mS7mbiym9G953vF1nVMEM=";
+      };
+
+      tracker = rustPlatform.buildRustPackage  {
+        version = "0.1.1";
+        pname = "tracker";
+        src = pkgs.fetchFromGitHub {
+          owner = "ShenMian";
+          repo = "tracker";
+          rev = "1c6b3b475af050da0cf0ac03e83487781b88213c";
+          hash = "sha256-w18zQ/OG1CeQQEW2e6pkHkYjTnaTWWOjPcaDJ//mAAg=";
+        };
+
+        cargoHash = "sha256-IoDzrW6JMmU0MeD5rXAhiHw1Nf0d2pHiIucsRk3raao=";
+        
       };
 
       binsider = rustPlatform.buildRustPackage {
@@ -357,6 +371,7 @@
         tetrs = tetrs;
         binsider = binsider;
         confetty = confetty;
+        tracker = tracker;
         plastic-tui = plastic-tui;
         fireplace = fireplace;
         lifecycler = lifecycler;
