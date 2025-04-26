ea6750000f57486eb8d47a9ab7eae929c75898a8
morgan <chengtz414@gmail.com>
2025-04-26 15:52:09 +0800

    rf2dfieldsolver: use sourceRoot

--- src/cpp/rf2dfieldsolver.nix
+++ src/cpp/rf2dfieldsolver.nix
@@ -4,19 +4,18 @@
   qmake,
   wrapQtAppsHook,
 }:
-let
-  repo = pkgs.fetchFromGitHub {
+pkgs.stdenv.mkDerivation rec {
+  pname = "RF2DFieldSolver";
+  version = "0.0.0";
+
+  src = pkgs.fetchFromGitHub {
     owner = "jankae";
     repo = "RF2DFieldSolver";
     rev = "1f3a9538a64daafea57c74f6bb085918d10cfeeb";
     hash = "sha256-u1sBlgQjgio/QvwVfwNCxmyFqY8sURZ9zRcWfwC49wQ";
   };
-in
-pkgs.stdenv.mkDerivation {
-  pname = "RF2DFieldSolver";
-  version = "0.0.0";
+  sourceRoot = "${src.name}/Software/RF2DFieldSolver";
 
-  src = "${repo}/Software/RF2DFieldSolver";
   nativeBuildInputs = [
     qmake
     wrapQtAppsHook
