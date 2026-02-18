3f454d9c879da86996e252507719eeee9e5bf4c5
morgan <chengtz414@gmail.com>
2026-02-18 18:14:34 +0800

    scopehal: update to v0.1.1

--- src/cpp/scopehal_apps.nix
+++ src/cpp/scopehal_apps.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 let
-  version = "0.1";
+  version = "0.1.1";
 in
 pkgs.scopehal-apps.overrideAttrs (oldAttr: {
   inherit version;
@@ -8,9 +8,34 @@ pkgs.scopehal-apps.overrideAttrs (oldAttr: {
     owner = "ngscopeclient";
     repo = "scopehal-apps";
     rev = "v${version}";
-    hash = "sha256-AfO6JaWA9ECMI6FkMg/LaAG4QMeZmG9VxHiw0dSJYNM=";
+    hash = "sha256-7ZXfxfRa+1fbMj2IDF/boNL/qCy4i9IyMnzIgOZunDw= ";
     fetchSubmodules = true;
   };
 
-  patches = oldAttr.patches or [ ] ++ [ ./dslogic_plus.patch ];
+  buildInputs =
+    oldAttr.buildInputs
+    # new dependencies of v0.1.1
+    # remove when nixpkg update scopehal to v0.1.1
+    ++ (with pkgs; [
+      libdatrie
+      libdeflate
+      libselinux
+      libsepol
+      libsysprof-capture
+      libthai
+      libuuid
+      libxdmcp
+      pcre2
+      lerc
+      xz
+      libwebp
+      libxkbcommon
+      libepoxy
+      libxtst
+    ]);
+  # Remove old v0.1 patches
+  patches = [
+    ./dslogic_plus.patch
+    ./remove_lsb_release.patch
+  ];
 })
